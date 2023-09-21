5ddba22a7223097e7f388b4e1bf4995352900bb0
walter-white03 <yuvraj10662@gmail.com>
2023-09-21 14:05:08 -0600

    assignment 2 in progress

---
+++ app/student_info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+    return(
+        <main>
+            <h2> Name : Yuvraj Singh</h2>
+            <h2>Course Section : CPRG 306 F</h2>
+            <Link href="https://github.com/skyhigh120923/cprg306-assignments">Github_Repository</Link>
+        </main>
+    )
+}
--- app/week2/page1.js
+++ app/week2/page1.js
@@ -2,12 +2,12 @@
 import Link from 'next/link'
 import StudentInfo from './student_info'
 
-export default function Home() {
+export default function Home1() {
   return (
     <main>
       <h1> CPRG 306: Web Development 2 - Assignments</h1>
       <StudentInfo></StudentInfo>
-      <Link to="app\week2\page1.js">week2</Link>
+      <Link href="www.google.com">week2</Link>
     </main>
    
   )
